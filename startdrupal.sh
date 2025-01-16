de767069619caeb84037894d2e444be77b31238f
David Belich <david@codekoalas.com>
2025-01-15 21:39:07 -0600

    init

---
+++ startdrupal.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+php -d memory_limit=256M web/core/scripts/drupal quick-start demo_umami
---
+++ test.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+./vendor/phpunit/phpunit/phpunit --bootstrap web/core/tests/bootstrap.php web/modules/custom/pdf_meta_extraction/tests/src/Unit/ProcessPdfTest.php
